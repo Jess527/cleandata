44e911bce825fb08bc6ffe0c01aa903adf7e7b6d
Jess527 <Jess527@users.noreply.github.com>
2014-07-21 10:12:46 -0400

    update descriptions

--- run_analysis.R
+++ run_analysis.R
@@ -4,13 +4,13 @@ dateDownloaded <- date() ##mark date
 data <- unzip(temp) ##unzip data file
 unlink(temp) ##close link to data url
 test <- read.table(data[15]) ##read in test data
-test_sub <- read.table(data[14]) ##subject labels
+test_sub <- read.table(data[14]) ##subject labels for test data
 test_act <- read.table(data[16]) ##read activity labels for test data
-test_full <- cbind(test_sub,test_act,test) ##add labels to test data in 1st column
+test_full <- cbind(test_sub,test_act,test) ##add subject & activity labels to test data in 1st & 2nd columns
 train <- read.table(data[27]) ##read in train data
-train_sub <- read.table(data[26]) ##read subject labels
-train_act <- read.table(data[28]) ##read activity labels
-train_full <- cbind(train_sub,train_act,train) ##add activity labels to train data in 1st column
+train_sub <- read.table(data[26]) ##read subject labels for train data
+train_act <- read.table(data[28]) ##read activity labels for train data
+train_full <- cbind(train_sub,train_act,train) ##add subject & activity labels to train data in 1st & 2nd columns
 full_DF <- rbind(test_full,train_full) ##all data together
 sub_order <- full_DF[order(full_DF[,1]),] ##order by subject number
 col <- read.table(data[2]) ##column descriptions
@@ -24,6 +24,6 @@ colNames <- gsub("(-)","",colNames) ##remove - from names
 colNames <- gsub("(m)","M",colNames) ##change to uppercase all M
 colNames <- gsub("(s)","S",colNames) ##change to uppercase all S
 colnames(final_DF) <- c("subjectID","activityLabel",colNames) ##adds names from colNames vector
-##calculate averages
+##calculate averages for each column by subject & activity
 melt_final <- melt(final_DF,id.vars=1:2)
 avg_DF <- dcast(melt_final, subjectID + activityLabel ~ variable,mean)
